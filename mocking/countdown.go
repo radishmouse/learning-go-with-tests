59fcc43665b964604f095e2dabf9df8986f9dea9
Chris Aquino <aquino.chris@pm.me>
2023-03-19 07:40:43 -0400

    Adds spy for writing and sleeping

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -7,6 +7,9 @@ import (
 	"time"
 )
 
+const write = "write"
+const sleep = "sleep"
+
 type Sleeper interface {
 	Sleep()
 }
@@ -17,6 +20,22 @@ type SpySleeper struct {
 
 func (s *SpySleeper) Sleep() {
 	s.Calls++
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+// This implements our own interface
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+// We're implementing the io.Writer interface with this one
+// and matching the function signature
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return // uses naked return for the zero values
 }
 
 type DefaultSleeper struct{}
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
-	t.Run("can do a thing", func(t *testing.T) {
+	t.Run("prints 3 to Go!", func(t *testing.T) {
 		buffer := &bytes.Buffer{} // why do I need the address-of?
 		spySleeper := &SpySleeper{}
 		Countdown(buffer, spySleeper) // ah...because I'm passing a pointer...
@@ -22,6 +23,25 @@ Go!`
 		}
 		if spySleeper.Calls != 3 {
 			t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+		}
+	})
+	t.Run("sleep before every print", func(t *testing.T) {
+		// I keep forgetting that I need the address of...
+		// because I want a pointer to a new SpyCountdownOperations
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("got %v want %v", want, spySleepPrinter.Calls)
 		}
 	})
 }
