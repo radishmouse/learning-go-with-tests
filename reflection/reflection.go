72e4b7d2dc5bc4118e2c1136ae90a258aca9c2aa
Chris Aquino <aquino.chris@pm.me>
2023-03-21 19:39:11 -0400

    Refactor to use a switch

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -7,16 +7,12 @@ func walk(x interface{}, fn func(input string)) {
 	// get a concrete value stored in the interface `x`
 	val := reflect.ValueOf(x)
 
-	// now...I'm going to receive more than one field.
-	// how do I count the number of fields?
 	for i := 0; i < val.NumField(); i++ {
-		// get the ith field from `val`, which must be a struct (else err)
-		field := val.Field(i)
-
-		if field.Kind() == reflect.String {
+		// :) I get to use the combo assign/eval trick:
+		switch field := val.Field(i); field.Kind() {
+		case reflect.String:
 			fn(field.String())
-		}
-		if field.Kind() == reflect.Struct {
+		case reflect.Struct:
 			walk(field.Interface(), fn)
 		}
 	}
